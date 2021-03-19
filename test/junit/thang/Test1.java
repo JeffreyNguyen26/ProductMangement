fb8253c9eaf438a8080078a5436a5ddea20eb8b3
Jeffrey Nguyen <jeffrey.nguyen98@gmail.com>
2021-03-19 17:42:04 +0700

    1

---
+++ test/junit/thang/Test1.java
@@ -0,0 +1,37 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package junit.thang;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author ACER
+ */
+public class Test1 {
+    
+    public Test1() {
+    }
+    
+    @Before
+    public void setUp() {
+    }
+    
+    @After
+    public void tearDown() {
+    }
+
+    // TODO add test methods here.
+    // The methods must be annotated with annotation @Test. For example:
+    //
+    // @Test
+    // public void hello() {}
+    
+    
+}
